5adfc8f2c0ece2893f4aeb43667ed96f9262e705
ProfAlfailakawi <ah_f@hotmail.com>
2026-08-13 09:04:20 +0300

    بسي

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+declare module '*.png'
+declare module '*.webp'
+declare module '*.jpg'
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_APPCHECK_SITE_KEY?: string
+  readonly VITE_MEDIA_AUDIO_BASE_URL?: string
+  readonly VITE_AUDIO_BASE_URL?: string
+}
